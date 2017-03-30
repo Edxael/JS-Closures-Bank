ee8dbe60d4b6fa5c57708b1651b11aa92c2af40a
Edxael <zerphyr.r@gmail.com>
2017-03-30 01:03:21 -0600

    Init of Bank

---
+++ code.js
@@ -0,0 +1,102 @@
+console.log("Bank 3");
+
+
+function bank(accNum, name, phone){
+
+    var account ={
+        accNumber: accNum,
+        accName: name,
+        accPhone: phone,
+        balance: 0,
+        dep1: function(num){ account.balance += num;},
+        wit2: function(num){ account.balance += num;}
+    }
+
+    return function(){
+        return account;
+    }
+}
+
+
+
+
+
+// ------------------  Code to Do Everything Else  ---------------------------------------------
+
+
+    // Data base to storate Accounts
+var dataBase ={ };
+
+
+    // Create the Account
+function createAccount(){
+    console.log("Creating Account:");
+    dataBase[document.getElementById("1").value] = new bank(document.getElementById("1").value,  document.getElementById("2").value,  document.getElementById("3").value );
+    console.log(dataBase);
+
+    for(var x = 1; x < 4; x++){
+        document.getElementById(x).value = "";
+    }
+}
+
+
+    // Deposit Funds
+function outDep(){
+    var num = parseInt(document.getElementById("per1").value);
+    console.log("\ninside outDep ");
+    console.log(typeof num);
+    var holder = dataBase.one();
+    console.log(holder);
+    console.log("Balance: " + holder.balance );
+
+    holder.balance += num;
+    console.log("Balance: " + holder.balance );
+}
+
+
+    // Display information on HTML
+function display(){
+    // Creating new HTML Paragrap with specific text and color
+    var holder = dataBase.one();
+    var new_P = document.createElement("p");
+    new_P.innerText = ("Balance: " + holder.balance );
+
+    var div1 = document.getElementById("div-p");
+    div1.insertBefore(new_P, div1.childNodes[0]);
+
+}
+
+
+
+
+
+
+
+
+
+// function createAccount(){
+//     console.log("Creating Account:");
+//     dataBase[document.getElementById("in-accn").value] = new bank(document.getElementById("in-accn").value,  document.getElementById("in-name").value,  document.getElementById("in-phon").value,  document.getElementById("in-bala").value  );
+//     console.log(dataBase);
+//
+//     document.getElementById("in-accn").value = "";
+// }
+
+
+
+
+
+// var alex = new bank("Alex", 123);
+//
+// function outDep(){
+//     console.log("inside outDep \n");
+//
+//     var holder = alex();
+//     console.log("\nBefore Function: ");
+//     console.log(holder.balance );
+//
+//     holder.dep1(5);
+//
+//     console.log("\nAftter Function: ");
+//     console.log(holder.balance);
+// }
